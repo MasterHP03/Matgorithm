1cf9cdd29b20a47e5d844c202b9cd9caf387777e
Matsalter <geniusyhp@gmail.com>
2026-05-11 15:18:46 +0900

    feat: implement obstacles and steering (avoidance)

--- main.py
+++ main.py
@@ -54,8 +54,9 @@ class Boid(Bird):
     w_align = 0.05
     w_coh = 0.01
     w_flee = 100
+    w_avoid = 10
 
-    def update(self, boid_list, pred_list=None):
+    def update(self, boid_list, pred_list=None, obstacle_list=None):
         neighbors = [other for other in boid_list
                      if other != self
                      and get_shortest_vec(self.pos, other.pos, W, H).length_squared() < self.r_sight_sq]
@@ -78,11 +79,42 @@ class Boid(Bird):
             self.vel += avg_dist * self.w_coh
 
         # 4. Flee/Evade
-        pred_in_sight = [pred for pred in pred_list
-                    if get_shortest_vec(self.pos, pred.pos, W, H).length_squared() < self.r_sight_sq]
-        if pred_in_sight:
-            flee_vec = sum((get_shortest_vec(pred.pos, self.pos, W, H) for pred in pred_in_sight), Vector2())
-            self.vel += flee_vec * self.w_flee
+        if pred_list:
+            pred_in_sight = [pred for pred in pred_list
+                        if get_shortest_vec(self.pos, pred.pos, W, H).length_squared() < self.r_sight_sq]
+            if pred_in_sight:
+                flee_vec = sum((get_shortest_vec(pred.pos, self.pos, W, H) for pred in pred_in_sight), Vector2())
+                self.vel += flee_vec * self.w_flee
+
+        # 5. Steering (avoid obstacle)
+        if obstacle_list:
+            for obs in obstacle_list:
+                to_obs = get_shortest_vec(self.pos, obs.pos, W, H)
+                forward_dir = self.vel.normalize()
+                forward_dist = to_obs.dot(forward_dir)
+                if 0 < forward_dist < self.r_sight:
+                    perp_dir = forward_dir.rotate(90)
+                    lateral_dist = to_obs.dot(perp_dir)
+                    if abs(lateral_dist) < obs.radius + 4:
+                        turn_force = perp_dir * self.w_avoid
+                        dist_force = max(1e-2, forward_dist - obs.radius - 4) # sharper steering if close
+
+                        self.vel = forward_dir * self.max_vel
+                        # opposite direction to obstacle
+                        if lateral_dist > 0:
+                            self.vel -= turn_force / dist_force
+                        else:
+                            self.vel += turn_force / dist_force
+                        # ignore other obstacles after deciding what obs to avoid
+                        break
+
+                # Hard Resolution (force collision)
+                actual_dist_sq = to_obs.length_squared()
+                min_dist = obs.radius + 4
+                if actual_dist_sq < min_dist * min_dist:
+                    actual_dist = math.sqrt(actual_dist_sq)
+                    overlap = min_dist - actual_dist
+                    self.pos -= to_obs / actual_dist * overlap
 
         # Normalize
         self.vel = self.vel.normalize() * self.max_vel \
@@ -123,17 +155,29 @@ class Predator(Bird):
         self.pos.x %= W
         self.pos.y %= H
 
+class Obstacle:
+    def __init__(self, pos, radius=3):
+        self.pos = pos
+        self.radius = radius
+
+    def draw(self, surface):
+        pygame.draw.circle(surface, (170, 170, 170), self.pos, self.radius)
+
 pygame.init()
 
 W, H = 800, 600
-n_boids = 100
+n_boids = 10
 boids = [Boid(Vector2(r.randint(0, W-1), r.randint(0, H-1)),
               Vector2(0, -1).rotate(r.uniform(0, 180)))
          for _ in range(n_boids)]
 n_preds = 1
 preds = [Predator(Vector2(r.randint(0, W-1), r.randint(0, H-1)),
-              Vector2(0, -1).rotate(r.uniform(0, 180)))
+                  Vector2(0, -1).rotate(r.uniform(0, 180)))
          for _ in range(n_preds)]
+n_obstacles = 20
+obstacles = [Obstacle(Vector2(r.randint(0, W-1), r.randint(0, H-1)),
+                      radius=15)
+             for _ in range(n_obstacles)]
 
 screen = pygame.display.set_mode((W, H))
 pygame.display.set_caption('GUI')
@@ -149,8 +193,8 @@ while running:
 
     grid = collections.defaultdict(list)
     GRID_SIZE = 75
-    MAX_GX = math.ceil(W // GRID_SIZE)
-    MAX_GY = math.ceil(H // GRID_SIZE)
+    MAX_GX = math.ceil(W / GRID_SIZE)
+    MAX_GY = math.ceil(H / GRID_SIZE)
 
     for boid in boids:
         gx = int(boid.pos.x // GRID_SIZE)
@@ -168,7 +212,7 @@ while running:
             wrap_y = (gy + j) % MAX_GY
             adjacent_boids.extend(grid[(wrap_x, wrap_y)])
 
-        boid.update(adjacent_boids, preds)
+        boid.update(adjacent_boids, preds, obstacles)
         boid.draw(screen)
 
     # --- update preds ---
@@ -182,6 +226,9 @@ while running:
         # Preventing reset to default direction when not moving
         pred.vel = pred.vel if curr_vel.length_squared() == 0 else curr_vel
         pred.draw(screen)
+
+    for obs in obstacles:
+        obs.draw(screen)
 
     pygame.display.flip()
 
